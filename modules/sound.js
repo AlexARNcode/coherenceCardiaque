19f9a4efb05a7b2b8f638100aadb38bf48dab272
Alex <53975649+AlexARNcode@users.noreply.github.com>
2020-11-26 12:28:35 +0100

    sound feature v0.1

---
+++ modules/sound.js
@@ -0,0 +1,16 @@
+var bgMusic = new Audio("/media/bgMusic.mp3");
+
+function playBgMusic() {
+    bgMusic.play();
+}
+
+function stopBgMusic() {
+    bgMusic.pause();
+}
+
+function playSound() {
+    const audio = new Audio("/media/bell1.wav");
+    audio.play();
+}
+
+export { playSound, playBgMusic, stopBgMusic };
--- modules/start.js
+++ modules/start.js
@@ -1,9 +1,11 @@
 import { disablePlay, enablePlay } from "./startBtn.js";
+import { playBgMusic, stopBgMusic, playSound } from "./sound.js";
 
 /*
 Main actions to do here
 1/ Disable play button for 5 minutes.
 2/ Play an audio file for 5 sec, then another audio file for 5 sec, etc etc... until 5 minutes total.
+2bis/ Play a background music for 5 minutes.
 3/ Show a progress bar during the 5 minutes.
 4/ After 5 minutes : Stop the progressbar, stop the sound, and enable the play button
 */
@@ -11,10 +13,14 @@ Main actions to do here
 function start() {
     // 1/ Disable play button for 5 minutes.
     disablePlay();
-    setTimeout(enablePlay, 3000); // 3000ms : set to 3s instead of 5 minutes for testing purposes
 
     // 2/ Play an audio file for 5 sec, then another audio file for 5 sec, etc etc... until 5 minutes total.
+    playBgMusic();
+    window.setInterval(playSound, 5000);
 
+    // Stop everything after 5 minutes
+    setTimeout(enablePlay, 15000);
+    setTimeout(stopBgMusic, 15000);
 }
 
 export { start };
